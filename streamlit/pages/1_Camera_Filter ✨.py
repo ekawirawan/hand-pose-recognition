e90a9f4475ada417a795c017ec5d7af084908c0b
ekawirawan <ekawirawan222@gmail.com>
2024-01-19 22:51:33 +0800

    change page config camera filter

--- streamlit/pages/1_Camera_Filter ✨.py
+++ streamlit/pages/1_Camera_Filter ✨.py
@@ -1,11 +1,13 @@
 import streamlit as st
-import sys
-from helpers.object_detection import realtime_video_detection
 
 st.set_page_config(
     page_title="Camera Filter",
     page_icon=":camera_with_flash:",
 )
+
+import sys
+from helpers.object_detection import realtime_video_detection
+
 
 sys.path.append("helpers")
 
